b0a5ba02f1432e11c8762f5cafd191d0860743c3
zpri <zoran.pribojevic@sbab.se>
2022-10-29 10:50:15 +0200

    Initial commit

---
+++ src/main/java/se/implementer/muaythaiservice/MuayThaiServiceApplication.java
@@ -0,0 +1,13 @@
+package se.implementer.muaythaiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuayThaiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuayThaiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/implementer/muaythaiservice/MuayThaiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.implementer.muaythaiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuayThaiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
