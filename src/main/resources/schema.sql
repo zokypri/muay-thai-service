33578b56a6cd81066fa3973792d3adb85028549e
zpri <zoran.pribojevic@sbab.se>
2022-11-01 20:32:17 +0100

    db structural changes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,46 +6,35 @@ CREATE TABLE FIGHTERS
     STAGE_NAME      VARCHAR2(30),
     HEIGHT_CM       VARCHAR2(30) not null,
     GENDER          VARCHAR2(30) not null,
-    TOTAL_FIGHTS    NUMBER not null,
-    WINS            NUMBER not null,
-    LOSSES          NUMBER not null,
-    DRAWS           NUMBER not null,
-    NO_CONTESTS     NUMBER not null,
-    WINS_KO         NUMBER not null,
-    WINS_DECISION   NUMBER not null,
-    LOSSES_KO       NUMBER not null,
-    LOSSES_DECISION NUMBER not null,
+    TOTAL_FIGHTS    NUMBER  ,
+    WINS            NUMBER  ,
+    LOSSES          NUMBER  ,
+    DRAWS           NUMBER  ,
+    NO_CONTESTS     NUMBER  ,
+    WINS_KO         NUMBER  ,
+    WINS_DECISION   NUMBER  ,
+    LOSSES_KO       NUMBER  ,
+    LOSSES_DECISION NUMBER  ,
     COUNTRY_ORIGIN  VARCHAR2(30) not null,
     COUNTRY_LIVING  VARCHAR2(30) not null,
     CITY            VARCHAR2(30) not null,
     CLUB            VARCHAR2(30) not null,
     FIGHT_ORG       VARCHAR2(30) not null,
-    RANKING         VARCHAR2(30) not null,
+    RANKING         VARCHAR2(30)  ,
     PRIMARY_WEIGHT  VARCHAR2(30) not null,
     BIRTH_DATE      DATE,
     FIGHTER_STATUS  VARCHAR2(30) not null
 );
 
-CREATE TABLE FIGHT_HISTORY
+CREATE TABLE FIGHT_INFO
 (
     FIGHT_ID        NUMBER primary key,
     FIGHTER_ID      NUMBER,
-    RESULT          VARCHAR2(30) not null,
     OPPONENT_ID     NUMBER,
-    ROUND           NUMBER,
+    FIGHT_NUMBER    NUMBER,
+    RESULT          VARCHAR2(30),
+    ROUND_KO        NUMBER,
     KO_TIME         VARCHAR2(30),
-    FIGHT_DAY       DATE,
-    LOCATION        VARCHAR2(30) not null,
-    ARENA           VARCHAR2(30) not null,
-    WEIGHT          VARCHAR2(30) not null,
-    FIGHT_ORG       VARCHAR2(30) not null,
-    foreign key (FIGHTER_ID) references FIGHTERS(FIGHTER_ID)
-);
-
-CREATE TABLE NEXT_SCHEDULED_FIGHT
-(
-    FIGHTER_ID      NUMBER primary key,
-    OPPONENT_ID     NUMBER,
     FIGHT_DAY       DATE,
     LOCATION        VARCHAR2(30) not null,
     ARENA           VARCHAR2(30) not null,
