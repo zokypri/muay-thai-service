91de705bf5b6513fc5fbdb1d8033eac71e20554b
zpri <zoky.zp@gmail.com>
2024-01-28 20:35:32 +0700

    added test for adding new fighter to DB

--- src/test/java/se/implementer/muaythaiservice/component/MuayThaiIT.java
+++ src/test/java/se/implementer/muaythaiservice/component/MuayThaiIT.java
@@ -4,16 +4,22 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Objects;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import se.implementer.muaythaiservice.model.dto.Gender;
+import se.implementer.muaythaiservice.repository.FighterRepository;
+import static se.implementer.muaythaiservice.util.testdata.TEST_URL.addFighter;
 import static se.implementer.muaythaiservice.util.testdata.TEST_URL.allActiveFightersByGender;
 import static se.implementer.muaythaiservice.util.testdata.TEST_URL.baseUrl;
 import static se.implementer.muaythaiservice.util.testdata.TEST_URL.fighterDetailsURL;
@@ -26,6 +32,9 @@ public class MuayThaiIT {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private FighterRepository fighterRepository;
 
     @Test
     void shouldFetchFighterOverview() throws Exception {
@@ -82,6 +91,47 @@ public class MuayThaiIT {
                 .andExpect(status().isOk());
 
         // TODO figure out how to fix the age otherwise the test will fail each time a birthday is had
+    }
+
+    @Test
+    void shouldAddFighter() throws Exception {
+
+        String filePathRequest = "__files/component/request/AddFighter.json";
+        String filePathResponse = "__files/component/response/AddFighter.json";
+
+        String requestContent = Files.readString(
+                Paths.get(
+                        Objects.requireNonNull(getClass().getClassLoader().getResource(filePathRequest))
+                                .toURI()
+                )
+        );
+
+        String responseContent = Files.readString(
+                Paths.get(
+                        Objects.requireNonNull(getClass().getClassLoader().getResource(filePathResponse))
+                                .toURI()
+                )
+        );
+
+        mockMvc.perform(
+                        post(baseUrl + addFighter)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestContent))
+                .andExpect(content().json(responseContent))
+                .andExpect(status().isOk());
+
+        var fighter = fighterRepository.findByFighterId(1);
+
+        assertTrue(fighter.isPresent());
+        assertEquals(1, fighter.get().getFighterId());
+        assertEquals("Zoran", fighter.get().getFighterStatus());
+        assertEquals(6, fighter.get().getTotalFights());
+        assertEquals(5, fighter.get().getWins());
+        assertEquals(1, fighter.get().getLosses());
+        assertEquals("Zoky", fighter.get().getStageName());
+        assertEquals("None", fighter.get().getClub());
+        assertEquals("ACTIVE", fighter.get().getFighterStatus());
+        assertEquals("MALE", fighter.get().getGender());
     }
 
     @Test
